43a2045e34920a165b9b33c1aa68035adf9b39bc
Stefan Koev <stefan.m.koev@gmail.com>
2020-01-08 13:18:25 +0200

    Add files via upload

---
+++ Snake Moves/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+
+namespace Snake_Moves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var matrixSize = Console.ReadLine()
+                .Split();
+
+            var N = int.Parse(matrixSize[0]);
+            var M = int.Parse(matrixSize[1]);
+
+            var matrix = new char[N, M];
+
+            string snake = Console.ReadLine();
+
+            int counter = 0;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+
+                if (row % 2 == 0)
+                {
+                    for (int col = 0; col < matrix.GetLength(1); col++)
+                    {
+                        matrix[row, col] = snake[counter++];
+
+                        if (counter == snake.Length)
+                        {
+                            counter = 0;
+                        }
+                    }
+                }
+                else
+                {
+                    for (int col = matrix.GetLength(1) - 1; col >= 0; col--)
+                    {
+                        matrix[row, col] = snake[counter++];
+
+                        if (counter == snake.Length)
+                        {
+                            counter = 0;
+                        }
+                    }
+                }
+            
+            }
+
+
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
